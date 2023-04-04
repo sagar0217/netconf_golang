23c73c9a4b53a509ccb15bdce1907232771232aa
sagar0217 <sagar.sharma0217@gmail.com>
2023-04-04 12:52:04 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module netconf
+
+go 1.20
+
+require (
+	github.com/Juniper/go-netconf v0.3.0
+	github.com/basgys/goxml2json v1.1.0
+	golang.org/x/crypto v0.7.0
+)
+
+require (
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/Juniper/go-netconf/netconf"
+	xj "github.com/basgys/goxml2json"
+	"golang.org/x/crypto/ssh"
+)
+
+const prefix = "veryuniqueattrprefix-"
+
+func main() {
+	http.HandleFunc("/editmodule", editModule)
+	http.HandleFunc("/getmodule", getModule)
+	http.HandleFunc("/addmodule", addModule)
+
+	log.Print(http.ListenAndServe(":8080", nil))
+}
+
+func addModule(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("File Upload Endpoint Hit")
+
+	// Parse our multipart form, 10 << 20 specifies a maximum
+	// upload of 10 MB files.
+	req.ParseMultipartForm(10 << 20)
+	// FormFile returns the first file for the given key `myFile`
+	// it also returns the FileHeader so we can get the Filename,
+	// the Header and the size of the file
+	file, handler, err := req.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+
+	tempFile, err := ioutil.TempFile("./", handler.Filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(tempFile.Name())
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// write this byte array to our temporary file
+	tempFile.Write(fileBytes)
+	// return that we have successfully uploaded our file!
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+
+	// out, err := exec.Command("sysrepoctl", "-i", fileName).Output()
+
+	// if err != nil {
+	// 	fmt.Printf("%s", err)
+	// } else {
+	// 	fmt.Println("Module stored successfully")
+	// }
+	// output := string(out[:])
+	// fmt.Println(output)
+}
+
+func getModule(w http.ResponseWriter, req *http.Request) {
+
+	session := startSSHSession()
+	if session == nil {
+		return
+	}
+	defer session.Close()
+
+	xml := `<led-data xlmns="urn:sysrepo:my"></led-data>`
+	r, err := session.Exec(netconf.MethodGet("subtree", xml))
+	if err != nil {
+		fmt.Println("err:1", err)
+		return
+	}
+
+	fmt.Println("r: ", r)
+	fmt.Println("\n\n\n\n")
+	fmt.Println("r.rawreply ", r.RawReply)
+
+	data := ConvertToJSON(r.RawReply) // // s = "<xmlns>Hi</ns>" --> []byte --> "{\"foo\":{\"baz\": [1,2,3]}}"( 12 342 42 12 )
+
+	JSONdata := make(map[string]interface{})
+	// json.Unmarshal([]byte(s), &JSONdata)
+	json.Unmarshal(data, &JSONdata)
+	// fmt.Println(JSONdata)
+	// yash, _ := json.Marshal(JSONdata)
+	// fmt.Println(string(yash))
+	w.Header().Set("Content-Type", "application/json")
+	// url := "localhost:8080"
+
+	// fmt.Fprint(w, string(yash))
+	json.NewEncoder(w).Encode(JSONdata)
+	//
+
+	// fmt.Println(">>>>>", r.RawReply)
+	// j := strings.Split(fmt.Sprint(r), "#203")
+	// fmt.Println(">>>>>>", strings.Split(j[1], ""))
+	// ConvertToJSON(fmt.Sprint(r))
+	// r, err = session.Exec(netconf.MethodGetConfig("running"))
+	// if err != nil {
+	// 	fmt.Println("err:2", err)
+	// 	return
+	// }
+
+	// fmt.Println("\n\n Running Confing \n")
+	// displayReply(r.RawReply)
+
+}
+func editModule(w http.ResponseWriter, req *http.Request) {
+	session := startSSHSession()
+	if session == nil {
+		return
+	}
+	defer session.Close()
+	// // Define the new YANG module content
+	//newModule := `<led-data xmlns="urn:sysrepo:my"><turned-on>17</turned-on></led-data>`
+	//newModule := ""
+	//xml.NewDecoder(req.Body).Decode(&newModule)
+	data, _ := ioutil.ReadAll(req.Body)
+	fmt.Println("@@@@@@", string(data))
+	// // Connect to the Netconf server
+	// conn, err := netconf.DialSSH("localhost:830", netconf.SSHConfigPassword("vvdn", os.Getenv("syspwd")))
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(session.ServerCapabilities)
+	// fmt.Println(session.SessionID)
+
+	// Add the new YANG module to the Netconf server
+	//_, err = conn.EditConfig(netconf.ConfigData(newModule), "merge", netconf.ConfigTargetRunning)
+	// 	data := `<led-data xmlns="urn:sysrepo:my">
+	//     <turned-on>17</turned-on>
+	// </led-data>`
+	_, err := session.Exec(netconf.MethodEditConfig("running", string(data)))
+	if err != nil {
+		fmt.Println("err:0", err)
+		return
+	}
+	// fmt.Println(r.RawReply)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, "New YANG module edited successfully")
+	//ConvertToJSON(fmt.Sprint(r))
+}
+
+func startSSHSession() *netconf.Session {
+	sshConfig := &ssh.ClientConfig{
+		User:            "netconf",
+		Auth:            []ssh.AuthMethod{ssh.Password("netconf")},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		//Timeout:         time.Duration(*timeout) * time.Second,
+	}
+	session, err := netconf.DialSSH("localhost:830", sshConfig)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	return session
+}
+
+func prettyPrint(b string) string {
+	var out bytes.Buffer
+	if err := json.Indent(&out, []byte(b), "", "  "); err == nil {
+		return string(out.Bytes())
+	}
+	return ""
+}
+
+func displayReply(rawReply string) {
+	xml := strings.NewReader(rawReply)
+	json, err := xj.Convert(xml)
+	if err != nil {
+		log.Fatal("Something went sore ... XML is invalid!")
+	}
+	fmt.Println(prettyPrint(json.String()))
+
+}
+
+func ConvertToJSON(xmlstring string) []byte {
+
+	xml := strings.NewReader(xmlstring)
+	// Decode XML document
+
+	json, err := xj.Convert(xml)
+	if err != nil {
+		panic("That's embarrassing...")
+		return nil
+	}
+
+	//	fmt.Println("#######", json.String())
+	return json.Bytes() // "  10 39 431"
+}
